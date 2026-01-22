b223a9339f438ee1bedd9174bbccd431fd87a3c5
Huyhoang120904 <hoangbeo1124@gmail.com>
2026-01-22 13:28:15 +0700

    feat (auth) : fix security config

--- auth-service/src/main/java/com/bondhub/authservice/dto/auth/response/TokenResponse.java
+++ auth-service/src/main/java/com/bondhub/authservice/dto/auth/response/TokenResponse.java
@@ -1,14 +1,9 @@
 package com.bondhub.authservice.dto.auth.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  * Token response DTO containing JWT tokens
  */
-public record TokenResponse(
-        @JsonProperty("access_token") String accessToken,
-
-        @JsonProperty("refresh_token") String refreshToken) {
+public record TokenResponse(String accessToken, String refreshToken) {
 
     public static TokenResponse of(String accessToken, String refreshToken) {
         return new TokenResponse(accessToken, refreshToken);
