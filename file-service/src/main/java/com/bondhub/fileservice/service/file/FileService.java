2144ac4089cb41fa9a90b005674d3a4c1fb28923
hoanghuy04 <hoangyeu365@gmail.com>
2026-04-21 21:45:08 +0700

    feat(file-user service): add presignedUrl functionality and use it to update usser avatar & background

--- file-service/src/main/java/com/bondhub/fileservice/service/file/FileService.java
+++ file-service/src/main/java/com/bondhub/fileservice/service/file/FileService.java
@@ -2,9 +2,12 @@ package com.bondhub.fileservice.service.file;
 
 import com.bondhub.common.dto.client.fileservice.FileUploadResponse;
 import com.bondhub.fileservice.dto.IngestUploadResponse;
+import com.bondhub.fileservice.dto.PresignFileRequest;
+import com.bondhub.fileservice.dto.PresignedUploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface FileService {
     FileUploadResponse uploadFile(MultipartFile file, String folder) throws IOException;
@@ -14,4 +17,6 @@ public interface FileService {
     byte[] downloadFile(String key);
 
     void deleteFile(String key);
+
+    List<PresignedUploadResponse> generatePresignedUrls(List<PresignFileRequest> requests);
 }
